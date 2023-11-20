d18e851363035d8a55dd573b8bbe9a98b8cd71bc
ayushichouhan04 <ayushi@complitech.net>
2023-11-20 18:16:01 +0530

    90% completed.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:role_id])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:role_id, :first_name, :last_name])
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :timezone, :current_password)}
   end
 
 end
--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,2 +1,17 @@
 module BlogsHelper
+  def convert_created_date(blog)
+    if current_user.timezone == "(GMT+05:30) Kolkata"
+      blog.created_at.in_time_zone('Chennai') 
+    else
+      blog.created_at
+    end
+  end
+
+  def convert_updated_date(blog)
+  	 if current_user.timezone == "(GMT+05:30) Kolkata"
+      blog.updated_at.in_time_zone('Chennai') 
+    else
+      blog.updated_at
+    end
+  end
 end
